c1f20e4e95311c4e297065e733a6a032d788019c
Denis Peganov <ddpeganov@avito.ru>
2026-03-11 13:19:09 +0500

    HW-3: refactor

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,26 +4,26 @@ import { Layout } from "./layout/layout";
 import { Restaurant } from "./restaurant";
 
 export const App = () => {
-  const [activeRestaurantId, setActiveRestaurantId] = useState(restaurants[0].id);
+  const [activeRestaurantId, setActiveRestaurantId] = useState(
+    restaurants[0].id,
+  );
 
   const activeRestaurant = restaurants.find(
-    (restaurant) => restaurant.id === activeRestaurantId
+    (restaurant) => restaurant.id === activeRestaurantId,
   );
 
   return (
     <Layout>
       <div>
         {restaurants.map((restaurant) => (
-          <button
-            onClick={() => setActiveRestaurantId(restaurant.id)}
-          >
+          <button onClick={() => setActiveRestaurantId(restaurant.id)}>
             Ресроран "{restaurant.name}"
           </button>
         ))}
       </div>
 
       <ul>
-          <Restaurant restaurant={activeRestaurant} />
+        <Restaurant restaurant={activeRestaurant} />
       </ul>
     </Layout>
   );

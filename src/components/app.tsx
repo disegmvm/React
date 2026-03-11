d454c4ebf1e1f3a4086dd4d4ddb46062449cc979
Denis Peganov <ddpeganov@avito.ru>
2026-03-11 13:16:18 +0500

    HW-3: Show active restaurant

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import { restaurants } from "./constants/mock";
 import { Layout } from "./layout/layout";
 import { Restaurant } from "./restaurant";
 
 export const App = () => {
+  const [activeRestaurantId, setActiveRestaurantId] = useState(restaurants[0].id);
+
+  const activeRestaurant = restaurants.find(
+    (restaurant) => restaurant.id === activeRestaurantId
+  );
+
   return (
     <Layout>
-      <ul>
+      <div>
         {restaurants.map((restaurant) => (
-          <Restaurant restaurant={restaurant} />
+          <button
+            onClick={() => setActiveRestaurantId(restaurant.id)}
+          >
+            Ресроран "{restaurant.name}"
+          </button>
         ))}
+      </div>
+
+      <ul>
+          <Restaurant restaurant={activeRestaurant} />
       </ul>
     </Layout>
   );

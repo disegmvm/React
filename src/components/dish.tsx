efa03b7aa8b8e0129b54160d5a10b79d30b4d260
Denis Peganov <ddpeganov@avito.ru>
2026-03-11 10:49:32 +0500

    HW-3: conditional rendering

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -2,22 +2,31 @@ import { useState } from "react";
 
 export const Dish = ({ dish }) => {
   const [count, setCount] = useState(0);
-  const add = () => {
-    setCount(count + 1);
+  const increase = () => {
+    if (count < 5) {
+      setCount(count + 1);
+    }
   };
-  const remove = () => {
-    setCount(count - 1);
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
   };
 
   return (
     <li>
       <p>Название: {dish.name}</p>
       <p>Цена: {dish.price}</p>
-      <p>Ингредиенты: {dish.ingredients.join(", ")}</p>
 
-      <button onClick={remove}>-</button>
+      {dish.ingredients.length > 0 ? (
+        <p>Ингредиенты: {dish.ingredients.join(", ")}</p>
+      ) : (
+        <p>Ингредиенты не указаны</p>
+      )}
+
+      <button onClick={decrease}>-</button>
       <span>{count}</span>
-      <button onClick={add}>+</button>
+      <button onClick={increase}>+</button>
     </li>
   );
 };
--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -6,22 +6,30 @@ export const Restaurant = ({ restaurant }) => {
       <h2>{restaurant.name}</h2>
 
       <p>Меню:</p>
-      <ul>
-        {restaurant.menu.map((dish) => (
-          <Dish dish={dish} />
-        ))}
-      </ul>
+      {restaurant.menu.length > 0 ? (
+        <ul>
+          {restaurant.menu.map((dish) => (
+            <Dish dish={dish} />
+          ))}
+        </ul>
+      ) : (
+        <p>Меню нет</p>
+      )}
 
       <p>Отзывы:</p>
-      <ul>
-        {restaurant.reviews.map((review) => (
-          <li>
-            <p>Пользователь: {review.user}</p>
-            <p>Отзыв: {review.text}</p>
-            <p>Оценка: {review.rating}</p>
-          </li>
-        ))}
-      </ul>
+      {restaurant.reviews.length > 0 ? (
+        <ul>
+          {restaurant.reviews.map((review) => (
+            <li key={review.id}>
+              <p>Пользователь: {review.user}</p>
+              <p>Отзыв: {review.text}</p>
+              <p>Оценка: {review.rating}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Отзывов нет</p>
+      )}
     </li>
   );
 };
